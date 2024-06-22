95a980005c56664b677673d1e01a2751441473e8
santo <santo@DESKTOP-PLTI0RD>
2024-06-21 21:15:20 -0300

    Adicionar arquivos de projeto.

---
+++ aspnetcrud/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using aspnetcrud.Data;
+using aspnetcrud.Models;
+using aspnetcrud.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcrud.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly AplicationDbContext dbContext;
+        public StudentsController(AplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add() 
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public async  Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribe = viewModel.Subscribe,
+            };
+           await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync(); 
+
+            return View();
+        }
+        [HttpGet]
+        public async  Task<IActionResult> List()
+        {
+            var students =await dbContext.Students.ToListAsync();
+        }
+    }
+}
---
+++ aspnetcrud/Data/AplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using aspnetcrud.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcrud.Data
+{
+  
+    public class AplicationDbContext: DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options):base(options) 
+        {
+            
+        }
+       public DbSet<Student> Students { get; set; } 
+    }
+}
