b265f02d16849fd8c04f73c14176480a52042d91
jonatas santos <santosjonaras@gmail.com>
2024-06-27 15:29:17 -0300

    new-add

--- aspnetcrud/Controllers/StudentsController.cs
+++ aspnetcrud/Controllers/StudentsController.cs
@@ -38,6 +38,47 @@ namespace aspnetcrud.Controllers
         public async  Task<IActionResult> List()
         {
             var students =await dbContext.Students.ToListAsync();
+            return View(students);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student =await dbContext.Students.FindAsync(id);
+            return View(student);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+
+            if (student is not null) {
+            
+              student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribe = viewModel.Subscribe;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x=>x.Id == viewModel.Id);
+
+            if (student is not null)
+            {
+
+               dbContext.Students.Remove(viewModel);
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
         }
     }
 }
